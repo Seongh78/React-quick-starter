bbbd32d91f23701c1a9d8272cd1cfb3031c0aeb2
김성훈 <seongh7800@douzone.com>
2018-11-26 13:45:53 +0900

    환경설정

---
+++ README.md
@@ -0,0 +1,19 @@
+# UIUX-Busan-Frontend-Boilerplate
+
+## 시작하기
+
+### 프로젝트 폴더에서 라이브러리를 설치한다
+
+#### `yarn install`
+
+### 프로젝트를 실행한다
+
+#### `yarn start`
+
+## 프로젝트 종속성
+
+### 설치라이브러리
+
+#### create-react-app ver2
+
+`https://github.com/facebook/create-react-app`
---
+++ src/pages/Main/MainPresenter.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
+
+/**
+ * MainPresenter
+ */
+const MainPresenter = () => (
+  <Fragment>
+    <Helmet>
+      <title>MainPage : BoilerPlate</title>
+    </Helmet>
+    <div>
+      <p>MainPage 입니다.</p>
+    </div>
+  </Fragment>
+);
+export default MainPresenter;
